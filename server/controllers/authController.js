c1d84b528d829762c035c97ffcd4b30a164cb44a
kvcsPat <smth.patrik@gmail.com>
2024-05-02 14:36:15 +0200

    created login logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -30,6 +30,12 @@ exports.signup = async (req, res, next) => {
       status: "success",
       message: "User registered successfully.",
       token,
+      user: {
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
     });
   } catch (error) {
     next(error);
@@ -38,5 +44,36 @@ exports.signup = async (req, res, next) => {
 
 // logging user
 exports.login = async (req, res, next) => {
-  res.send("Login");
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) return next(new createError("User not found!", 404));
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return next(new createError("Invalid email or password!", 401));
+    }
+
+    // assign JWT (json web token)
+    const token = jwt.sign({ _id: user._id }, "secretkey123", {
+      expiresIn: "90d",
+    });
+
+    res.status(200).json({
+      status: "success",
+      token,
+      message: "Logged in successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
